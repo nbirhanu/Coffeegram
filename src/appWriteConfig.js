34306a395a327863883fbec50e15f034cfaeae7e
nbirhanu <nbirhanu80@gmail.com>
2023-08-15 12:23:39 +0300

    Querying database items

--- src/appWriteConfig.js
+++ src/appWriteConfig.js
@@ -1,5 +1,9 @@
-import { Client } from 'appwrite'
+import { Client, Databases } from 'appwrite'
+export const DATABASE_ID = '64db37fdaedd9d23a02b'
+export const COLLECTION_MESSAGE_ID = '64db380e52df8be3cbf4'
+export const PROJECT_ID = '64db199642508803a239'
 const client = new Client()
 
 client.setEndpoint('https://cloud.appwrite.io/v1').setProject('64db199642508803a239')
+export const databases = new Databases(client)
 export default client
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { databases, DATABASE_ID, COLLECTION_MESSAGE_ID, PROJECT_ID } from '../appWriteConfig'
 
 const Room = () => {
+	useEffect(function () {
+		getMessage()
+	}, [])
+	const getMessage = async function () {
+		const response = await databases.listDocuments(DATABASE_ID, COLLECTION_MESSAGE_ID)
+		console.log('response', response)
+	}
 	return <div>Room</div>
 }
 
